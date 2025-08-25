fd0e9876e0c32b38e2c01f36f17048f607d2cf9f
Martin Domig <martin@domig.net>
2025-08-25 08:34:03 +0200

    set session cookie config for CORS credentials

--- backend/app.py
+++ backend/app.py
@@ -14,6 +14,9 @@ def create_app():
     db_path = os.path.join(db_dir, 'chesscrew.sqlite3')
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # Set session cookie for cross-origin credentials
+    app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+    app.config['SESSION_COOKIE_SECURE'] = True
 
     db.init_app(app)
     with app.app_context():
